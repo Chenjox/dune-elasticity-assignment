a46030c6fa5061cf9e0d7c8223a372be24323ec5
David Diepelt (K) <david.diepelt@gmx.net>
2024-08-10 17:22:53 +0200

    changing things around

--- dune/biw4-07/biw4-07.hh
+++ dune/biw4-07/biw4-07.hh
@@ -10,32 +10,22 @@
 // add your classes here
 namespace Dune {
     namespace BIW407 {
-    
-        /**
-            \brief Computes the linear strain bmatrix from the shape function gradient
+        
+        /** \brief Compute linearised strain at the identity from a given displacement gradient.
+         *
+         *  \param grad The gradient of the direction in which the linearisation is computed.
+         *  \param strain The tensor to store the strain in.
          */
-        template <int dim, int symdim = (dim*(dim-1)/2), class field_type=double>
-        void linearStrainBMatrix(const Dune::FieldVector<field_type, dim>& grad, const Dune::FieldMatrix<field_type, symdim, dim>& bMatrix) {
-            bMatrix = 0;
-            if (dim == 2) {
-                bMatrix[0][0] = grad[0];
-                bMatrix[1][1] = grad[1];
-                bMatrix[2][0] = grad[1];
-                bMatrix[2][1] = grad[0];
+        template <int dim, class field_type=double>
+        static void linearisedStrain(const Dune::FieldMatrix<field_type, dim, dim>& grad, Dune::FieldMatrix<double,dim,dim>& strain) {
+            for (int i=0; i<dim ; ++i)
+            {
+                strain(i,i) = grad[i][i];
+                for (int j=i+1; j<dim; ++j)
+                    strain(i,j) = 0.5*(grad[i][j] + grad[j][i]);
             }
         }
 
-        template <int dim, int symdim = (dim*(dim-1)/2), class field_type=double>
-        void linearStrainCMatrix(const Dune::FieldMatrix<field_type, symdim, symdim>& CMatrix, const field_type first_lame, const field_type second_lame) {
-            CMatrix = 0;
-            if (dim == 2){
-                CMatrix[0][0] = 2.0*second_lame + first_lame;
-                CMatrix[0][1] = first_lame;
-                CMatrix[1][1] = 2.0*second_lame + first_lame;
-                CMatrix[1][0] = first_lame;
-                CMatrix[2][2] = 2.0*second_lame;
-            }
-        }
 
     }
 }
--- src/biw4-07.cc
+++ src/biw4-07.cc
@@ -1,7 +1,10 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 
+#include "dune/biw4-07/biw4-07.hh"
 #include "dune/common/fmatrix.hh"
+#include <cstddef>
+#include <vector>
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
@@ -36,6 +39,7 @@
 // These are functions defined on a grid
 #include <dune/functions/gridfunctions/discreteglobalbasisfunction.hh>
 #include <dune/functions/gridfunctions/gridviewfunction.hh>
+#include <dune/functions/gridfunctions/gridfunction.hh>
 
 // For Plotting in Paraview
 #include <dune/grid/io/file/vtk/subsamplingvtkwriter.hh>
@@ -48,9 +52,11 @@ using namespace Dune;
 // therefore we do a template
 // LocalView: LocalView of Geometry
 // 
-template<class LocalView>
-void assembleElementStiffnessMatrix(const LocalView& localView,
-Matrix<double>& elementMatrix)
+template<class LocalView, class GridFunction>
+void assembleElementStiffnessMatrix(
+  const LocalView& localView,
+  Matrix<double>& elementMatrix,
+  const GridFunction& displacements)
 {
   // what type of element do I have?
   using Element = typename LocalView::Element;
@@ -69,6 +75,9 @@ Matrix<double>& elementMatrix)
   using namespace Indices;
   const auto& displacementLocalFiniteElement
     = localView.tree().child(0).finiteElement();
+
+  // How many Dofs, on the finite element in total?
+  int num_dofs = displacementLocalFiniteElement.localBasis().size();
 
   // take necessary integration order.
   int order = 2*(dim*displacementLocalFiniteElement.localBasis().order());
@@ -90,26 +99,40 @@ Matrix<double>& elementMatrix)
     displacementLocalFiniteElement.localBasis().evaluateJacobian(quadPoint.position(),referenceGradients);
     // Compute the shape function gradients on the grid element
     std::vector<FieldVector<double,dim> > gradients(referenceGradients.size());
-    for (size_t i=0; i<gradients.size(); i++)
+    for (size_t i=0; i<gradients.size(); i++){
+      gradients[i] = 0.0;
       jacobianInverseTransposed.mv(referenceGradients[i][0], gradients[i]);
+    }
 
-    for (size_t i=0; i<displacementLocalFiniteElement.size(); i++)
-      for (size_t j=0; j<displacementLocalFiniteElement.size(); j++ )
-        for (size_t k=0; k<dim; k++)
-        {
-          // We traverse the tree, get the $k$th-child an get their indezes
-          // Question: Does this only work, because the function space basis is the same for each subspace?
-          size_t row = localView.tree().child(k).localIndex(i);
-          size_t col = localView.tree().child(k).localIndex(j);
+    // A two dimensional list of FieldMatrizes, 
+    // storing the strains of every shape function of every strain.
+    // seems inefficient to me, but it works.
+    std::vector<std::array<FieldMatrix<double, dim, dim>,dim>> deltaLinStrain(num_dofs);
+    // Loop over the Dofs
+    for (size_t i=0; i<num_dofs; i++)
+    {
+      for (size_t k=0; k<dim; k++)
+      {
+        // 
+        FieldMatrix<double,dim,dim> deformationGradient(0);
+        deformationGradient[k] = gradients[i];
+        //elementMatrix[row][col] += ( gradients[i] * gradients[j] )* quadPoint.weight() * integrationElement;
 
-          // here is the bilinear form of the differential equation!
-          // Compute the stresses
-          FieldMatrix<double, dim, dim*(dim+1)/2> bmatrixReal;
-
-          elementMatrix[row][col] += ( gradients[i] * gradients[j] )
-          * quadPoint.weight() * integrationElement;
+        FieldMatrix<double, dim, dim> linearisedStrains(0);
+        for (size_t m=0; m<dim; m++)
+          for (size_t n=0; n<dim; n++){
+            linearisedStrains[m][n] += 0.5 * (deformationGradient[m][n] + deformationGradient[m][n]);
         }
-    
+      }
+    }
+
+    // get the _real_ deformation gradient
+    typename GridFunction::DerivativeType localConfGrad;
+    if (displacements.isDefinedOn(element))
+        displacements.evaluateDerivativeLocal(element, quadPoint, localConfGrad);
+    else
+      displacements.evaluateDerivative(geometry.global(quadPoint),localConfGrad);
+
   }
 }
 
@@ -155,8 +178,8 @@ Matrix& matrix, const MultiIndex& row, const MultiIndex& col)
 }
 
 // Assemble the Laplace stiffness matrix on the given grid view
-template<class Basis, class Matrix>
-void assembleStiffnessMatrix(const Basis& basis, Matrix& matrix)
+template<class Basis, class Matrix, class GridFunction>
+void assembleStiffnessMatrix(const Basis& basis, Matrix& matrix, GridFunction& displacements)
 {
   // Set matrix size and occupation pattern
   setOccupationPattern(basis, matrix);
@@ -173,7 +196,7 @@ void assembleStiffnessMatrix(const Basis& basis, Matrix& matrix)
     // Now let’s get the element stiffness matrix
     // A dense matrix is used for the element stiffness matrix
     Dune::Matrix<double> elementMatrix;
-    assembleElementStiffnessMatrix(localView, elementMatrix);
+    assembleElementStiffnessMatrix(localView, elementMatrix, displacements);
     // Add element stiffness matrix onto the global stiffness matrix
     for (size_t i=0; i<elementMatrix.N(); i++)
     {
@@ -198,6 +221,10 @@ int main(int argc, char** argv)
   MPIHelper& helper = Dune::MPIHelper::instance(argc, argv);
   constexpr int dim = 2;
 
+
+  /////////////////////////////////////////////////////////
+  // GRID DATA
+  /////////////////////////////////////////////////////////
   // With this the Grid is Read and the geometry is setup.
   // We use YaspGrid for starters
   using Grid = Dune::YaspGrid<2>;
@@ -210,6 +237,12 @@ int main(int argc, char** argv)
 
   using namespace Functions::BasisFactory;
 
+  using Coordinate = GridView::Codim<0> ::Geometry::GlobalCoordinate;
+
+  //////////////////////////////////////////////////////////
+  // Function Space Basis
+  //////////////////////////////////////////////////////////
+
   // Linear Ansatz Order
   constexpr int p = 1;
 
@@ -217,46 +250,52 @@ int main(int argc, char** argv)
   auto lagrangeBasis = makeBasis(gridView,
                                    power<dim>(
                                    lagrange<p>(),
-                                   blockedInterleaved()));
+                                   blockedInterleaved())); 
+  // Blocked interleaved allows for efficent sparsity structure.
   
 
+  ///////////////////////////////////////////////////////////
+  // Setting up Storage and Matrizes
+  ///////////////////////////////////////////////////////////
+
+  // Every Node has `dim` Degrees of Freedom.
+  using DisplacementRange = FieldVector<double,dim>;
   // Multiindex: Nodes x Num Dimension
-  using DisplacementVector = BlockVector<FieldVector<double,dim>>;
+  using DisplacementVector = BlockVector<DisplacementRange>;
   // Double the Multiindex: One Block for each Node of Dimension (dim x dim)
-  using Matrix = BCRSMatrix<FieldMatrix<double,dim,dim>>;
+  using Matrix = BCRSMatrix<DisplacementRange>;
+  // Marking displacement
+  using DisplacementBitVector = std::vector<std::array<char,dim> >;
 
-  // Declare the type of the rhs
+  // Declare the Boundary Vector, and the Right-Hand Side
+  DisplacementBitVector isBoundary;
   DisplacementVector rhs;
-
-  // initialize the datastructure
-  auto rhsBackend = Functions::istlVectorBackend(rhs);
-  rhsBackend.resize(lagrangeBasis); // and allocate memory for it.
-
-  // Initialization of it's entries
-  rhs = 0;
-
-  // Declare the type of the stiffness matrix
   Matrix stiffnessMatrix;
 
-  std::cout << "Assembling step." << std::endl;
+  // 'rhsBackend' now connects rhs with `dune-functions`
+  auto rhsBackend = Functions::istlVectorBackend(rhs);
+  rhsBackend.resize(lagrangeBasis);
 
-  assembleStiffnessMatrix(lagrangeBasis, stiffnessMatrix);
-
-
-  std::cout << "Dirichlet step." << std::endl;
-  ////
-  // Incorporate Dirichlet values.
-  ////
-  using DisplacementBitVector = std::vector<std::array<char,dim> >;
-  DisplacementBitVector isBoundary;
+  rhs = 0;
+  DisplacementVector x = rhs;
 
   auto isBoundaryBackend = Functions::istlVectorBackend(isBoundary);
   isBoundaryBackend.resize(lagrangeBasis);
+
+  auto displacementFunction = Functions::makeDiscreteGlobalBasisFunction<DisplacementRange>(lagrangeBasis, x);
 
   using namespace Indices;
   for (auto&& b0i : isBoundary)
     for (std::size_t j=0; j<b0i.size(); ++j)
       b0i[j] = false;
+
+  // Declare the type of the stiffness matrix
+
+  std::cout << "Assembling step." << std::endl;
+
+  assembleStiffnessMatrix(lagrangeBasis, stiffnessMatrix, displacementFunction);
+
+  std::cout << "Dirichlet step." << std::endl;
 
   ////
   // Convenience Function for Boundary DOFs
@@ -268,19 +307,21 @@ int main(int argc, char** argv)
   
   ////
   // Genau den hier machen
-  using Coordinate = GridView::Codim<0> ::Geometry::GlobalCoordinate;
-  using DisplacementRange = FieldVector<double,dim>;
 
   // Funktion die den Dirichlet-Rand vorgibt
   auto&& g = [](Coordinate x)
     {
       return DisplacementRange{0.0, (x[1] - 1.0 < 1e-8) ? 1.0 : 0.0};
     };
+  
+  // modify rhs to incorporate dirichlet values
   Functions::interpolate(lagrangeBasis,
       rhs,
       g,
       isBoundary);
-
+      
+  //stiffnessMatrix.mmv(x,rhs);
+  
   auto localView = lagrangeBasis.localView();
   for(const auto& element : elements(gridView))
   {
@@ -299,8 +340,10 @@ int main(int argc, char** argv)
     }
   }
 
-  // that way the Dirichlet entries are already correct.
-  DisplacementVector x = rhs;
+  //////////////////////////////////////////////////
+  // Solving the ~linear~ system
+  //////////////////////////////////////////////////
+
   // Turn the matrix into a linear operator
   std::cout << "Solving step." << std::endl;
 
@@ -319,11 +362,7 @@ int main(int argc, char** argv)
   InverseOperatorResult statistics;
   // Solve!
   cg.apply(x, rhs, statistics);
-
-  using VelocityRange = FieldVector<double,dim>;
   
-  auto displacementFunction
-    = Functions::makeDiscreteGlobalBasisFunction<DisplacementRange>(lagrangeBasis, x);
 
   SubsamplingVTKWriter<GridView> vtkWriter(
     gridView,
