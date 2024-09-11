bbcfeabbb98796fa460b42f9cf5edce09b1d6bbd
David Diepelt (K) <david.diepelt@gmx.net>
2024-09-11 21:34:42 +0200

    adding secondPK

--- dune/biw4-07/currentConfigMaterial.hh
+++ dune/biw4-07/currentConfigMaterial.hh
@@ -13,6 +13,8 @@ namespace Dune {
 
             virtual double strainEnergyDensity(const Dune::FieldMatrix<double, dim, dim>& deformationGradient) const;
         
+            virtual void secondPKStresses(const Dune::FieldMatrix<double, dim, dim>& deformationGradient, Dune::FieldMatrix<double, dim, dim>& PKIIStress) const;
+
             virtual void cauchyStresses(const Dune::FieldMatrix<double, dim, dim>& deformationGradient, Dune::FieldMatrix<double, dim, dim>& cauchyStress) const;
 
             virtual void cauchyStressInkrement(const Dune::FieldMatrix<double, dim, dim>& deformationGradient, const Dune::FieldMatrix<double, dim, dim>& symGradient, Dune::FieldMatrix<double, dim, dim>& cauchyStressInkrement) const;
